16d074f9f652d4d0f97eed7ad0121be63cc372e4
unknown <bojan.komazec@gmail.com>
2013-06-11 12:13:29 +0100

    Commiting changes in proj file and main

--- Mono/HelloWorld/HelloWorld/Main.cs
+++ Mono/HelloWorld/HelloWorld/Main.cs
@@ -7,6 +7,9 @@ namespace HelloWorld
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
+
+            YieldTester yieldTester = new YieldTester();
+            yieldTester.Test();
 		}
 	}
 }
